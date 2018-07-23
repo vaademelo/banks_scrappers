989a64b1a4d2451212eb1585838a0a717e7fd213
Vinicius Adaime Alves de Melo <vaademelo@gmail.com>
2018-07-23 10:33:00 -0300

    changes to the api of NubankScrapper in order to make it more generic

--- scrappers/nubank_scrapper.rb
+++ scrappers/nubank_scrapper.rb
@@ -2,37 +2,39 @@ class NubankScrapper < BaseScrapper
   def initialize
     visit_url('https://app.nubank.com.br/#/login')
   end
-
-  def scrape
-    login
-    open_bills
+  #args: {login: <>, password: <>, account: <>}
+  def scrape(args = {})
+    @args = args
+    login(args[:login], args[:password])
+    fetch_bills
   end
 
   private
 
-  def login
-    browser.fill_in('cpf', with: ENV['NUBANK_LOGIN'])
-    browser.fill_in('senha', with: ENV['NUBANK_PASSWORD'])
+  def login(login, password)
+    browser.fill_in('cpf', with: login)
+    browser.fill_in('senha', with: password)
     browser.click_button('entrar')
   end
 
-  def open_bills
+  def fetch_bills
     browser.click_link('Faturas')
 
     browser.all(:css, '.md-header md-tab').each do |bill|
-      amount = bill.find(:css, '.amount').text
-      period = bill.find(:css, '.period').text
+      account =
+      amount  = bill.find(:css, '.amount').text.gsub(/([^0-9])/, '').to_i
+      period  = bill.find(:css, '.period').text
 
-      @nubank_bill = NubankBill.new(amount: amount,
-                                    period: parse_period(period),
-                                    charges: [])
+      @nubank_bill   = Bill.find_by(period: parse_period(period))
+      @nubank_bill ||= Bill.new(account: @args[:account],
+                                amount: amount,
+                                period: parse_period(period))
 
-      get_bill_expenses(bill)
-      byebug
+      fetch_charges(bill)
     end
   end
 
-  def get_bill_expenses(bill)
+  def fetch_charges(bill)
 
     # Wait for resource to be loaded
     browser.has_content?('Valores em R$')
@@ -48,18 +50,25 @@ class NubankScrapper < BaseScrapper
     sleep(3)
 
     browser.all(:css, '.charge').each do |charge|
-      byebug
-      @nubank_bill.charges << Charge.new(date: charge.find(:css, '.date').text.split(' ').first,
-                                         description: charge.find(:css, '.description').text,
-                                         amount: Money.new(charge.find(:css, '.amount').text.gsub(/[,.]/, '').to_i, 'BRL'))
+      date = charge.find(:css, '.date').text.split(' ').first
+      description = charge.find(:css, '.description').text
+      amount = charge.find(:css, '.amount').text.gsub(/([^0-9])/, '').to_i
+
+      if @nubank_bill.charges.where(date: date, description: description, amount: amount).blank?
+        @nubank_bill.charges.build(date: date,
+                                   description: description,
+                                   amount: amount)
+      end
     end
+
+    @nubank_bill.save!
   end
 
   def parse_period(period)
     if period.length == 3
-      "#{Time.now.year}_#{MONTHS_TRANSLATIONS[period]}"
+      "#{Time.now.year}-#{MONTHS_TRANSLATIONS[period]}"
     else
-      "20#{period.split(' ').last}_#{MONTHS_TRANSLATIONS[period.split(' ').first]}"
+      "20#{period.split(' ').last}-#{MONTHS_TRANSLATIONS[period.split(' ').first]}"
     end
   end
 end
